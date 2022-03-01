9114c5747ba91f91992d71952934c304a37af665
Stevie Gill <29311273+caffeinated-pixels@users.noreply.github.com>
2022-03-01 10:50:17 -0500

    add components & css from scrim

--- src/App.js
+++ src/App.js
@@ -1,3 +1,27 @@
+import React from 'react'
+import boxes from './boxes'
+import Box from './Box'
+
 export default function App() {
-  return <h1>Hello World!</h1>
+  const [squares, setSquares] = React.useState(boxes)
+
+  function toggle(id) {
+    setSquares((prevSquares) => {
+      return prevSquares.map((square) => {
+        return square.id === id ? { ...square, on: !square.on } : square
+      })
+    })
+  }
+
+  const squareElements = squares.map((square) => {
+    return (
+      <Box key={square.id} on={square.on} toggle={() => toggle(square.id)} />
+    )
+  })
+
+  return (
+    <main>
+      <div className='box-container'>{squareElements}</div>
+    </main>
+  )
 }
---
+++ src/Box.js
@@ -0,0 +1,7 @@
+export default function Box(props) {
+  const styles = {
+    backgroundColor: props.on ? 'turquoise' : 'red',
+  }
+
+  return <div className='box' style={styles} onClick={props.toggle}></div>
+}
